8c830a5105dd51cfb32b91f45d547d8b0309d178
Danny Forst <forst056@gmail.com>
2012-12-28 20:11:58 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cardcadia::Application.config.secret_token = 'c65ef631ad2699f15effbb5df24c77fdc0d912bc5095025a77d06fa66ee839a37f8e9a76bcf585293c72410430c874148a294f3db00c6c905df2d0bd030ccf28'
